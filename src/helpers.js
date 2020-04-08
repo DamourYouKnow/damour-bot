cac9bc195815191da5413f696cca619a74c00f90
damouryouknow <bailey@damour.xyz>
2020-04-08 14:39:38 -0400

    Add relative path to helpers module

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,11 @@
 
 const fs = require('fs');
+const path = require('path');
 const yaml = require('js-yaml');
 
-const config = yaml.safeLoad(fs.readFileSync('config.yml'));
+const config = yaml.safeLoad(
+    fs.readFileSync(path.resolve(__dirname, '../config.yml'))
+);
 
 module.exports = {};
 
